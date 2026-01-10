822117eb33a2d53b8553e145b9c15afa15d30e33
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-01-10 23:15:09 +0100

    Added methods for placing mines and uncovering tiles

--- minesweeper_game/game.py
+++ minesweeper_game/game.py
@@ -1,3 +1,5 @@
+import random
+from collections import deque
 from enum import Enum, auto
 
 
@@ -34,14 +36,33 @@ class MinesweeperGame:
 
         self.cols = cols
         self.rows = rows
+        self.mine_count = mine_count
         self.grid = [[Tile() for _ in range(cols)] for _ in range(rows)]
+        self.mines_placed_ = False
 
-    def calculate_values(self):
-        for i, row in enumerate(self.grid):
-            for j, tile in enumerate(row):
-                if tile.is_mine:
-                    for x, y in get_neighbours(j, i, self.cols, self.rows):
-                        self.grid[y][x].value += 1
+    def place_mine_(self, x, y) -> bool:  # true if succeeded
+        if self.grid[y][x].is_mine:
+            return False
+
+        self.grid[y][x].is_mine = True
+
+        for nx, ny in get_neighbours(x, y, self.cols, self.rows):
+            self.grid[ny][nx].value += 1
+
+        return True
+
+    def place_mines(self, safe_spots=[]):
+        safe_spots = set(safe_spots)
+        potential_spots = [
+            (x, y)
+            for x in range(self.cols)
+            for y in range(self.rows)
+            if (x, y) not in safe_spots
+        ]
+        mine_spots = random.sample(potential_spots, self.mine_count)
+
+        for x, y in mine_spots:
+            self.place_mine_(x, y)
 
     def cycle_covered_state(self, x, y) -> bool:  # true if succeeded
         tile = self.grid[y][x]
@@ -65,5 +86,28 @@ class MinesweeperGame:
         if tile.state == TileState.FLAGGED:
             return True
 
-        tile.state = TileState.UNCOVERED
-        return not tile.is_mine
+        if not self.mines_placed_:
+            self.place_mines([(x, y)])
+            self.mines_placed_ = True
+
+        if tile.is_mine:
+            print("MINE")
+            tile.state = TileState.UNCOVERED
+            return False
+
+        to_uncover = deque([(x, y)])
+        while to_uncover:
+            cur_x, cur_y = to_uncover.pop()
+            cur_tile = self.grid[cur_y][cur_x]
+
+            cur_tile.state = TileState.UNCOVERED
+
+            if cur_tile.value == 0:
+                for nx, ny in get_neighbours(cur_x, cur_y, self.cols, self.rows):
+                    if self.grid[ny][nx].state in (
+                        TileState.COVERED,
+                        TileState.QUESTIONED,
+                    ):
+                        to_uncover.append((nx, ny))
+
+        return True
