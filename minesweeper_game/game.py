d2ff1b9358f04f6fb24d842106812fb6f4f29cd9
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-01-10 19:34:34 +0100

    Create MinesweeperGame class

---
+++ minesweeper_game/game.py
@@ -0,0 +1,29 @@
+from enum import Enum, auto
+
+
+class TileState(Enum):
+    COVERED = auto()
+    MARKED = auto()
+    QUESTIONED = auto()
+    UNCOVERED = auto()
+
+
+class Tile:
+    def __init__(self):
+        self.is_bomb = False
+        self.value = 0
+        self.state = TileState.COVERED
+
+
+class MinesweeperGame:
+    def __init__(self, cols, rows, mine_count):
+        assert cols >= 9
+        assert rows >= 9
+        assert mine_count >= 10
+        assert cols <= 30
+        assert rows <= 24
+        assert mine_count <= (cols - 1) * (rows - 1)
+
+        self.cols = cols
+        self.rows = rows
+        self.grid = [[Tile() for _ in range(cols)] for _ in range(rows)]
