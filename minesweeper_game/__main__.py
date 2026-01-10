a7aab621cd0518a3d8fba8b2f7ce607dfe4da6a1
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-01-10 19:36:12 +0100

    Created basic game window initialization

--- minesweeper_game/__main__.py
+++ minesweeper_game/__main__.py
@@ -1,5 +1,26 @@
+import pygame
+
+from .window import MinesweeperWindow
+from .game import MinesweeperGame
+
+ROWS = 16
+COLS = 16
+MINES = 40
+
+SAFE_START_NEIGHBOURS = False  # make all neighbours of starting tile safe?
+
+
 def main() -> None:
-    pass
+    pygame.init()
+
+    game = MinesweeperGame(30, 16, 99)
+    window = MinesweeperWindow(game)
+
+    running = True
+    while running:
+        running = window.tick()
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
