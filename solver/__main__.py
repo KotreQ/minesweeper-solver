89cebcc0031e8cb86fa0cbac5b365edde9644552
Bartłomiej Kochanek <bkochanek4@gmail.com>
2026-02-21 17:14:37 +0100

    Modified solver tests and changed difficulty to intermediate

--- solver/__main__.py
+++ solver/__main__.py
@@ -1,37 +1,51 @@
 import time
 
+import numpy as np
+
 from game.game import GameState, MinesweeperGame
 
 from .solver import MinesweeperSolver
+from .utils.cli import clear_console
 
-COLS = 10
-ROWS = 10
-MINES = 10
+COLS = 16
+ROWS = 16
+MINES = 40
 
 
 def main() -> None:
 
     ### TEST
 
-    print("TEST")
-    test_results = [0, 0]
-    for i in range(1000):
+    clear_console()
+
+    TEST_COUNT = 100
+    test_times = np.zeros((TEST_COUNT), np.float64)
+    test_results = np.zeros((TEST_COUNT), np.bool_)
+    for i in range(TEST_COUNT):
         test_game = MinesweeperGame(COLS, ROWS, MINES)
         test_solver = MinesweeperSolver(test_game)
 
+        test_start = time.time()
         while not test_solver.finished:
             test_solver.make_move()
+        test_time = time.time() - test_start
+        test_times[i] = test_time * 1000  # in ms
+        test_results[i] = test_game.state == GameState.WON
 
-        if test_game.state == GameState.WON:
-            test_results[1] += 1
-        else:
-            test_results[0] += 1
-
-        print(f"WON {test_results[1]}:{test_results[0]} LOST", end="\r")
+        print(f"\rTESTING: {(i + 1) / TEST_COUNT * 100:.1f}%", end="")
     print("")
 
-    print("Waiting for demo...")
-    time.sleep(3)
+    clear_console()
+
+    win_count = np.count_nonzero(test_results)
+
+    print(f"Win rate: {win_count / TEST_COUNT * 100:.1f}%")
+    print(f"Median win time: {np.median(test_times[test_results]):.1f}ms")
+    print(f"Median loss time: {np.median(test_times[~test_results]):.1f}ms")
+
+    print("")
+    print("Running demo...")
+    time.sleep(5)
 
     ### DEMO
 
@@ -40,9 +54,11 @@ def main() -> None:
     solver = MinesweeperSolver(game)
 
     while not solver.finished:
-        time.sleep(0.5)
+        time.sleep(0.2)
         solver.make_move()
+        clear_console()
         solver.print_grid()
+        print(f"Game state: {game.state.name}")
 
 
 if __name__ == "__main__":
